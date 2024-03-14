cb850101fc08ef1b93a9cb8cc3865751def3865a
daviapolin17 <davi.apolinario277@gmail.com>
2024-03-14 16:17:42 -0300

    melhor ainda

--- js/script.js
+++ js/script.js
@@ -5,14 +5,30 @@ function validar() {
     const graduacao = document.getElementById("graduacao");
     const campoSelect = document.getElementById("campo_select");
 
-    if (nome.value == "" || sobrenome.value == "" || anoNascimento.value == "" || graduacao.value == "" || campoSelect.value == "") {
-        alert("Erro: Preencha todos os campos!");
+    if (nome.value == "") {
+        alert("Erro: O campo Nome está em branco!");
+        return false;
+    }
+    if (sobrenome.value == "") {
+        alert("Erro: O campo Sobrenome está em branco!");
+        return false;
+    }
+    if (anoNascimento.value == "") {
+        alert("Erro: O campo Ano de Nascimento está em branco!");
+        return false;
+    }
+    if (graduacao.value == "") {
+        alert("Erro: O campo Graduação Pretendida está em branco!");
+        return false;
+    }
+    if (campoSelect.value == "") {
+        alert("Erro: O campo Campo Select está em branco!");
         return false;
     }
 
     const anoAtual = new Date().getFullYear();
-    if (anoAtual - parseInt(anoNascimento.value) > 15) {
-        alert("Erro: Apenas alunos com até 15 anos podem se cadastrar!");
+    if (anoAtual - parseInt(anoNascimento.value) < 15) {
+        alert("Erro: Apenas alunos com mais de 15 anos podem se cadastrar!");
         return false;
     }
 
@@ -22,7 +38,7 @@ function validar() {
         <p>Nome: ${nome.value} ${sobrenome.value}</p>
         <p>Ano de Nascimento: ${anoNascimento.value}</p>
         <p>Graduação Pretendida: ${graduacao.value}</p>
-        <p>Campo Select: ${campoSelect.value}</p>
+        <p>Cidade: ${campoSelect.value}</p>
     `;
 
     return false; // Evita o envio do formulário
